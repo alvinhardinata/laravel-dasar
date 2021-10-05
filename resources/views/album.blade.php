833d10b2a92f3240c2df043001ea55aa14df3155
alvinhardinata <you@example.com>
2021-10-05 19:34:42 +0700

    pertemuan keempat

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -44,8 +44,8 @@
         <section class="pt-4">
             <div class="container px-lg-5">
                 <!-- Page Features-->
-                <div class="row gx-lg-5">
                 @for ($i = 0; $i <= 10; $i++)
+                <div class="row gx-lg-5">
                     <div class="col-lg-6 col-xxl-4 mb-5">
                         <div class="card bg-light border-0 h-100">
                             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
@@ -54,8 +54,8 @@
                                 <p class="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
                             </div>
                         </div>
-                    @endfor
                     </div>
+                    @endfor
                     <div class="col-lg-6 col-xxl-4 mb-5">
                         <div class="card bg-light border-0 h-100">
                             <div class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
